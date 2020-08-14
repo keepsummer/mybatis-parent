f75d97ee3a29b032523401e066b14137e751bb8b
李志敏 <915377975@qq.com>
2020-08-14 15:07:44 +0800

    学习live template

---
+++ src/main/java/com/lizhimin/mybatis/Temple.java
@@ -0,0 +1,20 @@
+package com.lizhimin.mybatis;
+
+public class Temple {
+
+    /**
+     * 名字
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private String age;
+
+
+
+
+
+
+}
