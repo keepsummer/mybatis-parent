cf0939a09925510abb1bc74e7da3e1ce003f4c78
李志敏 <915377975@qq.com>
2020-08-14 16:25:26 +0800

    test

---
+++ src/main/java/com/lizhimin/mybatis/Temple.java
@@ -0,0 +1,49 @@
+package com.lizhimin.mybatis;
+
+public class Temple {
+
+    /**
+     * 名字
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private String age;
+    
+    /**
+     * 体重
+     */
+    private String weight;
+    
+    /**
+     * 性别
+     */
+    private String sex;
+
+    /**
+     * 兴趣爱好
+     */
+    private String hobby;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+
+
+}
